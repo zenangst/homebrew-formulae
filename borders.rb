b10d87546f903c5917b542336ca166e1f59616ac
Christoffer Winterkvist <christoffer@winterkvist.com>
2024-09-18 01:21:43 +0200

    Add borders.rb

---
+++ borders.rb
@@ -0,0 +1,47 @@
+# typed: false
+
+# frozen_string_literal: true
+
+# borders.rb
+class Borders < Formula
+  env :std
+  desc "A window border system for macOS"
+  homepage "https://github.com/FelixKratz/JankyBorders"
+  url "https://github.com/FelixKratz/JankyBorders/archive/refs/tags/v1.6.0.tar.gz"
+  sha256 "e48dcb432c9dabf8358a126bd1b4b22bc36587808d23fa7bb3c5e1dcecbf05d3"
+  license "GPL-3.0-only"
+  head "https://github.com/zenangst/JankyBorders.git", branch: "main"
+
+  depends_on :macos => :sonoma
+
+  def clear_env
+    ENV.delete("CFLAGS")
+    ENV.delete("LDFLAGS")
+    ENV.delete("CXXFLAGS")
+  end
+
+  def install
+    clear_env
+    (var/"log/jankyborders").mkpath
+    system "make"
+
+    system "codesign", "--force", "-s", "-", "#{buildpath}/bin/borders"
+    bin.install "#{buildpath}/bin/borders"
+
+    man.mkpath
+    man1.install "#{buildpath}/docs/borders.1"
+  end
+
+  service do
+    run "#{opt_bin}/borders"
+    environment_variables PATH: std_service_path_env, LANG: "en_US.UTF-8"
+    keep_alive true
+    process_type :interactive
+    log_path "#{var}/log/borders/borders.out.log"
+    error_log_path "#{var}/log/borders/borders.err.log"
+  end
+
+  test do
+    echo "To test your installation run: borders \n"
+  end
+end
